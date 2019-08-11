5cd1428225bd2af04babcd3d8eb261a4b7635ce3
Johannes Wünsche <johannes.wuensche97@gmail.com>
2019-08-11 16:41:41 +0200

    Add AsyncProgressView

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use std::sync::mpsc::{self, Receiver};
 use cursive::{wrap_impl, Cursive, Printer, Rect, Vec2};
 use cursive::event::{Event, EventResult, AnyCb};
 use cursive::direction::Direction;
@@ -7,7 +6,6 @@ use cursive::views::{TextView};
 use cursive::utils::markup::StyledString;
 use cursive::theme::PaletteColor;
 use interpolation::Ease;
-
 fn clamp(number: f64) -> f64 {
     if number < 0.0 {
         0.0
@@ -17,6 +15,7 @@ fn clamp(number: f64) -> f64 {
         number
     }
 }
+use crossbeam::channel::{Sender, Receiver, unbounded};
 
 /// Repeat the string `s` `n` times by concatenating.
 pub fn repeat_str<S: Into<String> + Clone>(s: S, n: usize) -> String {
@@ -91,6 +90,143 @@ impl<T: View> ViewWrapper for DelayView<T> {
     wrap_impl!(self.view: T);
 }
 
+pub struct AsyncProgressView<T: View + Send> {
+    view: Option<T>,
+    loading: TextView,
+    animation_fn: Box<dyn Fn(usize, f32) -> StyledString>,
+    width: Option<usize>,
+    rx_v: Receiver<T>,
+    rx_p: Receiver<f32>,
+}
+
+impl <T: View + Send + Sized> AsyncProgressView<T> {
+    pub fn new<F>(
+        siv: &Cursive,
+        width: Option<usize>,
+        creator: F
+    ) -> Self
+    where
+        F: FnOnce(Sender<f32>) -> T + Send + 'static
+    {
+        let (sx_v, rx_v) = unbounded();
+        let (sx_p, rx_p) = unbounded();
+        let sink = siv.cb_sink().clone();
+        std::thread::spawn(move || {
+            sx_v.send(creator(sx_p)).unwrap();
+            sink.send(Box::new(|_: &mut Cursive| {}))
+        });
+        let other_sink = siv.cb_sink().clone();
+        // ⛏
+        std::thread::spawn(move || {
+            loop {
+                std::thread::sleep(std::time::Duration::from_millis(33));
+                other_sink.send(Box::new(|_: &mut Cursive| {})).unwrap();
+            }
+        });
+
+        Self {
+            view: None,
+            loading: TextView::new(""),
+            animation_fn: Box::new(progress),
+            width: width,
+            rx_v: rx_v,
+            rx_p: rx_p,
+        }
+    }
+}
+
+impl <T: View + Send + Sized> View for AsyncProgressView<T> {
+
+    fn draw(&self, printer: &Printer) {
+        match self.view {
+            Some(ref view) => view.draw(printer),
+            None => self.loading.draw(printer),
+        }
+    }
+
+    fn layout(&mut self, vec: Vec2) {
+        if self.view.is_none() {
+            match self.rx_p.try_recv() {
+                Ok(v) => {
+                    if let Some(width) = self.width {
+                        self.loading.set_content((self.animation_fn)(width, v));
+                    }
+                },
+                Err(_) => {},
+            }
+        }
+
+        match self.view {
+            Some(ref mut view) => view.layout(vec),
+            None => self.loading.layout(vec),
+        }
+    }
+
+    fn needs_relayout(&self) -> bool {
+        if self.view.is_none() {
+            return true;
+        }
+
+        match self.view {
+            Some(ref view) => view.needs_relayout(),
+            None => self.loading.needs_relayout(),
+        }
+    }
+
+    fn required_size(&mut self, constraint: Vec2) -> Vec2 {
+        if self.view.is_none() {
+            match self.rx_v.try_recv() {
+                Ok(view) => self.view = Some(view),
+                Err(_) => {},
+            }
+
+            if self.width.is_none() {
+                self.width = Some(constraint.x);
+            }
+        }
+
+        match self.view {
+            Some(ref mut view) => view.required_size(constraint),
+            None => self.loading.required_size(constraint),
+        }
+    }
+
+    fn on_event(&mut self, ev: Event) -> EventResult {
+        match self.view {
+            Some(ref mut view) => view.on_event(ev),
+            None => self.loading.on_event(ev),
+        }
+    }
+
+    fn call_on_any<'a>(&mut self, sel: &Selector, cb: AnyCb<'a>) {
+        match self.view {
+            Some(ref mut view) => view.call_on_any(sel, cb),
+            None => self.loading.call_on_any(sel, cb),
+        }
+    }
+
+    fn focus_view(&mut self, sel: &Selector) -> Result<(), ()> {
+        match self.view {
+            Some(ref mut view) => view.focus_view(sel),
+            None => self.loading.focus_view(sel),
+        }
+    }
+
+    fn take_focus(&mut self, source: Direction) -> bool {
+        match self.view {
+            Some(ref mut view) => view.take_focus(source),
+            None => self.loading.take_focus(source),
+        }
+    }
+
+    fn important_area(&self, view_size: Vec2) -> Rect {
+        match self.view {
+            Some(ref view) => view.important_area(view_size),
+            None => self.loading.important_area(view_size),
+        }
+    }
+}
+
 pub struct AsyncView<T: View + Send> {
     view: Option<T>,
     loading: TextView,
@@ -135,6 +271,7 @@ impl AsyncViewBuilder {
     }
 }
 
+
 impl<T: View + Send> AsyncView<T> {
     // TODO: add timeout parameter
     fn new<F>(
@@ -148,7 +285,7 @@ impl<T: View + Send> AsyncView<T> {
     {
         // trust me, I'm an engineer
         let sink = siv.cb_sink().clone();
-        let (tx, rx) = mpsc::channel();
+        let (tx, rx) = unbounded();
         std::thread::spawn(move || {
             tx.send(creator()).unwrap();
             sink.send(Box::new(|_: &mut Cursive| {}))
@@ -188,6 +325,8 @@ impl<T: View + Send + Sized> View for AsyncView<T> {
 
     fn layout(&mut self, vec: Vec2) {
         self.loading.set_content(self.animation[self.pos].clone());
+
+        // Here communication with receiving end of channel
         self.pos += 1;
         if self.pos >= self.animation.len() {
             self.pos = 0;
