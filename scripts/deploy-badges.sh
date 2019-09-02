bf763392fca586ccad98b355378f99cb610ba08f
Fin Christensen <christensen.fin@gmail.com>
2019-09-02 17:48:31 +0200

    Update badge deploy script

--- scripts/deploy-badges.sh
+++ scripts/deploy-badges.sh
@@ -23,23 +23,30 @@ fi
 (
     cd "$(git rev-parse --show-toplevel)/target/shields" || die "cannot find project root!"
     repo="https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_REPO_SLUG}.git"
-    tmp_dir=$(mktemp -d -t cursive-async-view-deploy-XXXXXXXX)
+    tmp_dir=$(mktemp -d -t cursive-multiplex-deploy-XXXXXXXX)
 
     try=0
     while :; do
-        git clone --branch gh-pages "$repo" "$tmp_dir"
+        if ! git clone --branch gh-pages "$repo" "$tmp_dir"
+        then
+            (
+                cd "$tmp_dir" || die "failed to enter temporary directory"
+                git init
+                git remote add origin "$repo"
+                git checkout -b gh-pages
+            )
+        fi
+
         cp -ar ./* "$tmp_dir"
 
         (
             cd "$tmp_dir" || die "failed to enter temporary directory"
             git add -A
             git commit -m "Travis CI badge deployment"
-            git push
+            git push origin gh-pages:gh-pages
         )
 
         result=$?
-        rm -rf "$tmp_dir"
-
         if [ "$result" -eq 0 ] || [ "$try" -ge 5 ]
         then
             break
@@ -47,4 +54,7 @@ fi
 
         try=$((try + 1))
     done
+
+    rm -rf "$tmp_dir"
+
 )
