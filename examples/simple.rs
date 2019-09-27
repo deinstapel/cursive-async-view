48d808ef8e3ce0faeb7b4fc7e982fc87aebcbb07
Fin Christensen <christensen.fin@gmail.com>
2019-09-27 18:16:34 +0200

    Adjust example to showcase how to use the async view with a background calculation

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,6 +1,9 @@
 extern crate cursive_async_view;
 
-use std::time::{Instant, Duration};
+use std::sync::mpsc::channel;
+use std::sync::mpsc::TryRecvError;
+use std::thread;
+use std::time::Duration;
 
 use cursive::{self, views::Dialog, views::Button, Cursive};
 use cursive_async_view::{AsyncView, AsyncState};
@@ -13,14 +16,22 @@ fn main() {
     // We can quit by pressing `q`
     siv.add_global_callback('q', Cursive::quit);
 
-    let time = Instant::now();
+    let (tx, rx) = channel();
+    thread::spawn(move || {
+        thread::sleep(Duration::from_secs(5));
+        tx.send("Yay! The content has loaded!").unwrap();
+    });
+
     let async_view = AsyncView::new(&mut siv, move || {
-        if time + Duration::from_secs(5) < Instant::now() {
-            AsyncState::Loaded(
-                Button::new("Yay! The content has loaded!", |_| {}).disabled()
-            )
-        } else {
-            AsyncState::Pending
+        // This function will be called several times.
+        // It should signal `Loaded` when the view is available for drawing.
+
+        // Do not run heavy calculations here!
+        // Instead look if the calculation is ready.
+        match rx.try_recv() {
+            Ok(msg) => AsyncState::Loaded(Button::new(msg, |_| {}).disabled()),
+            Err(TryRecvError::Empty) => AsyncState::Pending,
+            Err(TryRecvError::Disconnected) => AsyncState::Error("Data not available".to_string()),
         }
     });
 
